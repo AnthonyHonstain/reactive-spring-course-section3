400e8a74dd08130ac72419d6bf0c63ef94c158f7
Anthony Honstain <honstain@gmail.com>
2022-10-15 16:14:25 -0700

    initial commit

---
+++ src/main/java/com/honstain/netflux/NetfluxApplication.java
@@ -0,0 +1,13 @@
+package com.honstain.netflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetfluxApplication.class, args);
+    }
+
+}
