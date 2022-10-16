1e4fe67254196bc2379635eac7f3b0221e3025a5
Anthony Honstain <honstain@gmail.com>
2022-10-15 17:12:34 -0700

    Section 3 Lesson 24

---
+++ src/main/java/com/honstain/netflux/controllers/MovieController.java
@@ -0,0 +1,37 @@
+package com.honstain.netflux.controllers;
+
+import com.honstain.netflux.domain.Movie;
+import com.honstain.netflux.domain.MovieEvent;
+import com.honstain.netflux.services.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+
+@RestController
+@RequestMapping("/movies")
+@RequiredArgsConstructor
+public class MovieController {
+
+    private final MovieService movieService;
+
+    @GetMapping("/{id}")
+    Mono<Movie> getMovieById(@PathVariable String id) {
+        return movieService.getMovieById(id);
+    }
+
+    @GetMapping
+    Flux<Movie> getAllMovies() {
+        return movieService.getAllMovies();
+    }
+
+    @GetMapping(value = "/{id}/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    Flux<MovieEvent> streamMovieEvents(@PathVariable String id) {
+        return movieService.streamMovieEvents(id);
+    }
+}
